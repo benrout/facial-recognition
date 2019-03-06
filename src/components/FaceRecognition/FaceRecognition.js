cafed8aeb8fba1278f2c7baccc5e93dd986c9519
Ben Rout <benadamrout@gmail.com>
2019-03-06 15:19:47 +0000

    Updates FaceRecognition component layout

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,10 +4,8 @@ import './FaceRecognition.css';
 const FaceRecognition = ({ imageURL, box }) => {
     return (
         <div className="center relative w-80 mt5">
-
-                <img id="inputImage" src={imageURL} alt="" className="w-100"/>
-                <div className="bounding-box" style={{top: box.top, right: box.right, bottom: box.bottom, left: box.left }}></div>
-
+            <img id="inputImage" src={imageURL} alt="" className="w-100" />
+            <div className="bounding-box" style={{ top: box.top, right: box.right, bottom: box.bottom, left: box.left }}></div>
         </div>
     )
 }
